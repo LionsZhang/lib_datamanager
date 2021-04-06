5afe057937ac6ebb3808778fc2d9c77cda8bc3db
lion <zqhusixia@163.com>
2021-04-06 14:37:49 +0800

    add datamanager lib

---
+++ app/src/main/java/com/wowly/common/lib/datamanager/DataManagerActivity.kt
@@ -0,0 +1,12 @@
+package com.wowly.common.lib.datamanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wowly.common.lib.datamanager.R.*
+
+class DataManagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
